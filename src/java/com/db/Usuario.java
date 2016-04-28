bd1adfce41371a6c9a7afebeab90d7efe1860d7c
Alejandro Ramirez Osorio <alejandro.ramirez@scitum.com.mx>
2016-04-28 13:27:46 -0500

    Clase para crear Usuarios

---
+++ src/java/com/db/Usuario.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.db;
+
+/**
+ *
+ * @author alejandro.ramirez
+ */
+public class Usuario {
+    // Tabla usuarios
+    int id;
+    String nombre;
+    String password;
+    String username;
+    
+    public Usuario(int id,String nombre, String password, String username) {
+        super();
+        this.id=id;
+        this.nombre = nombre;
+        this.password = password;
+        this.username = username;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    } 
+}
