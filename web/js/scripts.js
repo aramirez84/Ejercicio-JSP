42c5dee2fa869b35249382d1f32af973cd375c43
Alejandro Ramirez Osorio <alejandro.ramirez@scitum.com.mx>
2016-05-05 14:56:57 -0500

    Agregando funcion Ajax para agregar usuarios

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,5 +1,94 @@
-$(document).ready(function(){
-    $("a").click(function(){
-        alert("Hola");
-    })
+$(function () {
+    var dialog, form,
+            nombre = $("#nombre"),
+            username = $("#username"),
+            password = $("#password"),
+            allFields = $([]).add(nombre).add(username).add(password),
+            tips = $(".validateTips");
+
+    function updateTips(t) {
+        tips
+                .text(t)
+                .addClass("ui-state-highlight");
+        setTimeout(function () {
+            tips.removeClass("ui-state-highlight", 1500);
+        }, 500);
+    }
+
+    function checkLength(o, n, min, max) {
+        if (o.val().length > max || o.val().length < min) {
+            o.addClass("ui-state-error");
+            updateTips("Length of " + n + " must be between " +
+                    min + " and " + max + ".");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function checkRegexp(o, regexp, n) {
+        if (!(regexp.test(o.val()))) {
+            o.addClass("ui-state-error");
+            updateTips(n);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function addUser() {
+        var valid = true;
+        allFields.removeClass("ui-state-error");
+
+        valid = valid && checkLength(nombre, "nombre", 3, 16);
+        valid = valid && checkLength(username, "username", 3, 16);
+        valid = valid && checkLength(password, "password", 5, 16);
+
+        valid = valid && checkRegexp(nombre, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
+        valid = valid && checkRegexp(username, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
+        valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
+
+        if (valid) {
+            $.post("ServletAgregar",{
+                nombre:nombre.val(),
+                username:username.val(),
+                password:password.val()
+            },function(data){
+                console.log(data);
+                location.reload();
+            });
+            dialog.dialog("close");
+        }
+        else
+        {
+           console.log("Error");
+        }
+        return valid;
+    }
+
+    dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Create an account": addUser,
+            Cancel: function () {
+                dialog.dialog("close");
+            }
+        },
+        close: function () {
+            form[ 0 ].reset();
+            allFields.removeClass("ui-state-error");
+        }
+    });
+
+    form = dialog.find("form").on("submit", function (event) {
+        event.preventDefault();
+        addUser();
+    });
+
+    $("#create-user").button().on("click", function () {
+        dialog.dialog("open");
+    });
 });
