6ed62bf91b7cd762033461f2412484e0ca8bfe3a
Alejandro <alexro2404@hotmail.com>
2016-05-10 13:58:07 -0500

    Reciebiendo parametros del formulario permisos

--- src/java/com/servlet/ServletAgregarPermisos.java
+++ src/java/com/servlet/ServletAgregarPermisos.java
@@ -31,19 +31,6 @@ public class ServletAgregarPermisos extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ServletAgregarPermisos</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ServletAgregarPermisos at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -72,7 +59,14 @@ public class ServletAgregarPermisos extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        int id=Integer.parseInt(request.getParameter("userID"));
+        String[] permisos=request.getParameterValues("permisos[]");
+        for (int i = 0; i <permisos.length; i++) {
+            out.println("permisos: "+permisos[i]);   
+        }
+        out.println("id: "+id);
+        
     }
 
     /**
